053c854d603275e481ff7f9fa028f0de611b8ed0
Kerriea-star <ojared511@gmail.com>
2022-02-27 13:00:07 +0300

    Created permissions at view level

--- data/views.py
+++ data/views.py
@@ -1,11 +1,13 @@
-from rest_framework import generics
+from rest_framework import generics, permissions # new (imported permissions from rest framework) 
 from .serializers import StudentSerializer
 from .models import Student
 
 class StudentList(generics.ListCreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,) # new
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
 
 class StudentDetail(generics.RetrieveUpdateDestroyAPIView):
+    permission_classes = (permissions.IsAuthenticated,) # new
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
